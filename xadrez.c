806a13c9233f6ea325105276316969b52a3dfc60
GabnCruz <nascimentogabriel1804@gmail.com>
2025-04-03 01:36:39 +0000

    Versão final entregue

--- xadrez.c
+++ xadrez.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
+void movertorre(int casas) //Recursividade para movimentação da torre
+{
+    if (casas > 0)
+    {
+        printf("Direita\n");
+        movertorre(casas - 1);
+    }
+}
+
+void moverbispo (int casas) //Recursividade para movimentação do bispo
+{
+    if (casas > 0)
+    {
+        printf("Cima Direita\n");
+        moverbispo(casas - 1);
+    }   
+}
+
+void moverrainha (int casas) //Recursividade para movimentação da rainha
+{
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        moverrainha(casas - 1);
+    }
+    
+}
+
 int main() {
     
     int torre = 5;
@@ -8,38 +36,37 @@ int main() {
     
     //Movimentação da Torre
     printf("Movimentação da Torre:\n");
-    while (torre >= 1)
-    {
-        printf("Direita\n");
-        torre --;
-    }
+    movertorre(torre);
 
-    //Movimentação do Bispo
-    printf("\nMovimentação do Bispo:\n");
-    do
+    //Movimentação do Bispo com loops aninhados
+    printf("\nMovimentação do Bispo (loop aninhado):\n");
+    for (int i = 1; i <= 5; i++)
     {
-        printf("Cima Direita\n");
-        bispo --;
-    } while (bispo >= 1);
+        printf("Cima "); //Eixo vertical
+
+        for (int j = 0; j < 1; j++)
+        {
+            printf("Direita"); //Eixo horizontal
+        }
+        printf("\n");
+    }
+    
+    //Movimentação do Bispo com Recursividade
+    printf("\nMovimentação do Bispo (Recursivo):\n");
+    moverbispo(bispo);
 
     //Movimentação da Rainha
     printf("\nMovimentação da Rainha:\n");
-    for (rainha; rainha >= 1; rainha --)
-    {
-        printf("Esquerda\n");
-    }
+    moverrainha(rainha);
 
     //Movimentação do Cavalo
     printf("\nMovimentação do Cavalo\n");
-    int i;
-    for (i = 0; i < 2; i++) {
-        printf("Baixo\n");
+    for (int i = 0; i < 2; i++) { // Movimento de duas casas para cima
+        printf("Cima\n");
     }
-    
-    int j = 0;
-    while (j < 1) {
-        printf("Esquerda\n");
-        j++;
+
+    for (int j = 0; j < 1; j++) { // Movimento de uma casa para a direita
+        printf("Direita\n");
     }
     
     return 0;
